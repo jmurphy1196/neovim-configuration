18151686a4f0acf0d5abb31ef251cc78e37a0b9f
jason murphy <jmurphy.1196@gmail.com>
2025-01-05 16:24:30 -0800

    adds discipline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
+
+local discipline = require("jmurphy1196.discipline")
+
+discipline.cowboy()
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 --  select all
